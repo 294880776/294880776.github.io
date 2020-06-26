30526b92e6265057a7865f2f35a219f4baa0c69f
秌月無痕 <58113242+294880776@users.noreply.github.com>
2020-06-26 22:51:35 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,9 +2,9 @@ let myImage = document.querySelector('img');
 
 myImage.onclick = function() {
     let mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/2-icon.jpg') {
+    if(mySrc === 'images/ayase.jpg') {
       myImage.setAttribute('src', 'images/2.jpg');
     } else {
-      myImage.setAttribute('src', 'images/2-icon.jpg');
+      myImage.setAttribute('src', 'images/ayase.jpg');
     }
 }
