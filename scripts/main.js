952d6265803bedd4a828f2fb83277240701795e5
秌月無痕 <58113242+294880776@users.noreply.github.com>
2020-06-26 23:00:47 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -7,4 +7,28 @@ myImage.onclick = function() {
     } else {
       myImage.setAttribute('src', 'images/ayase.jpg');
     }
+};
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('请输入你的名字。');
+  if (!myName || myName === null) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = '欢迎进入294880776的个人网站，' + myName;
+  }
 }
+
+if (!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = '欢迎进入294880776的个人网站，' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+};
