52d98be8d7cc0278c65323df1f23425e01a844ee
秌月無痕 <58113242+294880776@users.noreply.github.com>
2020-06-26 22:50:01 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,9 +2,9 @@ let myImage = document.querySelector('img');
 
 myImage.onclick = function() {
     let mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/2-icon.png') {
-      myImage.setAttribute('src', 'images/2.png');
+    if(mySrc === 'images/2-icon.jpg') {
+      myImage.setAttribute('src', 'images/2.jpg');
     } else {
-      myImage.setAttribute('src', 'images/2-icon.png');
+      myImage.setAttribute('src', 'images/2-icon.jpg');
     }
 }
