643a663421fd5fc6d214d05ab41178a34e8e8880
秌月無痕 <58113242+294880776@users.noreply.github.com>
2020-06-26 23:14:24 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,10 +2,10 @@ let myImage = document.querySelector('img');
 
 myImage.onclick = function() {
     let mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/ayase.jpg') {
+    if(mySrc === 'images/1.jpg') {
       myImage.setAttribute('src', 'images/2.jpg');
     } else {
-      myImage.setAttribute('src', 'images/ayase.jpg');
+      myImage.setAttribute('src', 'images/1.jpg');
     }
 };
 
