b7a92e27f4af885b9057ca3a15b186dc0cc91561
秌月無痕 <58113242+294880776@users.noreply.github.com>
2020-06-26 22:43:53 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@ let myImage = document.querySelector('img');
 
 myImage.onclick = function() {
     let mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/firefox-icon.png') {
+    if(mySrc === 'images/2-icon.png') {
       myImage.setAttribute('src', 'images/2.png');
     } else {
       myImage.setAttribute('src', 'images/2-icon.png');
