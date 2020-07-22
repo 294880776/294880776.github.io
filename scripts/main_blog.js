564db36f47d9af6505887309c5f85197f386855d
秌月無痕 <58113242+294880776@users.noreply.github.com>
2020-07-22 23:21:20 +0800

    Delete main_blog.js

--- scripts/main_blog.js
+++
@@ -1,7 +0,0 @@
-// 添加滚动事件
-window.addEventListener('scroll', function () {
-    //获取元素
-    var header = document.querySelector("header");
-    // 添加类
-    header.classList.toggle("sticky", window.scrollY > 0)
-});
